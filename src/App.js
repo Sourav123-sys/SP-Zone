482e5ea68456c327fb4f108602c9e8e5886887d7
Sourav123 <khansourav58@gmail.com>
2022-04-17 14:28:31 +0600

    notfound-added

--- src/App.js
+++ src/App.js
@@ -24,9 +24,9 @@ function App() {
         <Route path = '/about' element={<About/>}></Route>
         <Route path='/login' element={<Login />}></Route>
         <Route path="/signup" element={<Register />}></Route> 
-        {/* <Route path='/contact' element={<Contact />}></Route> */}
-        {/* <Route path='/checkout' element={<CheckOut />}></Route> */}
-        {/* <Route path="*" element={<NotFound />}></Route> */}
+        <Route path='/contact' element={<Contact />}></Route>
+        <Route path='/checkout' element={<CheckOut />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
       </Routes>
       <Footer/>
     </div>
